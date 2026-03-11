6d70752113501f772cd9d7b981c14fdf9828033c
Cedric Aquino <caquino@paterostechnologicalcollege.edu.ph>
2026-03-11 09:09:53 +0800

    Add files via upload

---
+++ grp6_aquino_ex3.py
@@ -0,0 +1,30 @@
+print("Hello, I'm Cedric Aquino! 1V")
+
+#python comments
+print("Hello, world")
+#My first simple script
+
+#python variables
+x=87
+y="Cedric"
+print(x, y)
+
+#Python-Variable Names
+charlz="Charlz"
+jirus="Jirus"
+jeshley="Jeshley"
+jerone="Jerone"
+print(charlz)
+print(jirus)
+print(jeshley)
+print(jerone)
+
+#Many Values to Multiple Variables
+x, y, z= 1, 2, 3
+print(x)
+print(y)
+print(z)
+
+#One Values to Multiple Variables
+x=y=z=1
+print(x)

79d6123bedf2b4f8250c42de497f85a9df52ec43
Cedric Aquino <caquino@paterostechnologicalcollege.edu.ph>
2026-03-17 00:19:56 +0800

    Add files via upload

---
+++ MDAS.py
@@ -0,0 +1,33 @@
+#Laboratory Exercise 4
+
+#1 Arithmetic operation
+
+print("Arihtmetic Operations")
+
+a=5
+b=100
+
+#Operations
+print("Addition", a + b)
+print("Subtraction", a - b)
+print("Division", a / b)
+print("Multiplication", a * b)
+print("Modulus", a % b)
+print("Exponent", a ** b)
+
+
+#2 Two Variables
+num1=25000000
+num2=250_000_00
+
+print(num1)
+print(num2)
+
+#3 Data Types
+a=1
+b=2.0
+c=3j
+
+print(type(a))
+print(type(b))
+print(type(c))
